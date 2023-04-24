06ce633b92919fce9ce32046020601e0a36c06ea
P. Yao <12227516+YaoPengCN@users.noreply.github.com>
2023-04-24 13:53:53 +0800

    feat: add example data and datasets module

--- src/pycounts/pycounts.py
+++ src/pycounts/pycounts.py
@@ -19,7 +19,7 @@ def load_text(input_file):
     --------
     >>> load_text("text.txt")
     """
-    with open(input_file, "r") as file:
+    with open(file=input_file, mode="r", encoding='utf-8') as file:
         text = file.read()
     return text
     
